4058d660cb33b7704e7e976658ed15d994ce419a
Todd Treece <toddtreece@gmail.com>
2015-02-10 18:39:50 -0500

    adds addrepo.sh

---
+++ addrepo.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+cat << "EOF"
+                                 .`
+                              -dMMs
+                             +MMMMMo
+                           .dMMMMMMN-
+                          +NMMMMMMMMd`
+                        `hMMMMMMMMMMMo
+                       -mMMMMMMMMMMMMN.
+                       dMMMMMMMMMMMMMMo
+  :hmmmmmmmmmmmmdhs/. `MMMMMMMMMMMMMMMh
+  sMMMMMMMMMMMMMMMMMMd+NMMMMMMMMMMMMMM+
+   /NMMMMMMMMMMMMMMMMMMMMMMMMs+NMMMMMm/+syyyso/-`
+    `hMMMMMMMMMMMMMMMMMMMMMMo  hMMMMMMMMMMMMMMMMMNhs+:.
+      /NMMMMMMMMMMMNmmNMMMMN. `mMMMMMMMMMMMMMMMMMMMMMMMNh-
+       .hMMMMMMMMMMh` `-sNMMs-hMMMMMMMMMMMMMMMMMMMMMMMMMM+
+         /mMMMMMMMMMNy+-./MMMMMMy:....oMMMMMMMMMMMMMMMNo.
+           :ymMMMMMMMMMMMMMMMMMNy//oymMMMMMMMMMMMMMNy:
+              .yMMMMMMMms:oMMNhNMMMMMMMMMMMMMMMMNh/`
+            .yMMMMMMMN/  .dMMy `sMMMMMMMMMMMNmy/`
+           /NMMMMMMMM:`-sMMMMM:  sMMMMMMs-..`
+          -NMMMMMMMMMNNMMMMMMMMs./MMMMMMMh`
+          mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMs
+         sMMMMMMMMMMMMMMMMhMMMMMMMMMMMMMMMN
+        :MMMMMMMMMMMMMMMN+ hMMMMMMMMMMMMMMN.
+       `dMMMMMMMMMMMMNh/`  `hMMMMMMMMMMMMMN.
+       /MMMMMMMMmhs+-        /dMMMMMMMMMMMN.
+       .hmdys/-                -sNMMMMMMMMN.
+                                 `:hNMMMMMN.
+                                    `+dMMMN`
+                                       ./+-
+
+  adding apt.adafruit.com to /etc/apt/sources.list
+
+EOF
+
+sleep 2
+
+if grep -Fq "adafruit" /etc/apt/sources.list; then
+  echo "adafruit repo already added to apt sources"
+else
+  # add apt repo to sources.list
+  echo "deb http://apt.adafruit.com/raspbian/ wheezy main" >> /etc/apt/sources.list
+
+  # import repo key
+  wget -O - -q https://apt.adafruit.com/apt.adafruit.com.gpg.key | apt-key add -
+fi
+
+# update package database
+apt-get update
